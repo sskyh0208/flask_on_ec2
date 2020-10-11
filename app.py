97bd876cfa6c5fdb98d3c1fa52edbe890f62d4d8
yoshihiro <sskyh0208@gmail.com>
2020-10-11 09:41:27 +0900

    ajaxの修正

--- app.py
+++ app.py
@@ -2,11 +2,11 @@
 from flaskr import create_app
 
 # windows時のみ
-import locale
-locale.setlocale(locale.LC_CTYPE, "Japanese_Japan.932")
+# import locale
+# locale.setlocale(locale.LC_CTYPE, "Japanese_Japan.932")
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
-    # app.run()
+    # app.run(host='0.0.0.0', port=80)
+    app.run()
